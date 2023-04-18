f9f81957526bdaa365ee3668fa97bf0523ee4cc1
changfeng <changfeng@gaoding.com>
2023-04-18 15:17:40 +0800

    feat: support git error fille and output panel syntax highlight

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(
-        'Congratulations, your extension "awesome-vscode-extension-boilerplate" is now active!',
-    );
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand(
-        'awesome-vscode-extension-boilerplate.helloWorld',
-        () => {
-            // The code you place here will be executed every time your command is executed
-            // Display a message box to the user
-            vscode.window.showInformationMessage(
-                'Hello World from Awesome VSCode Extension Boilerplate!',
-            );
+    // support git error file syntax highlight
+    vscode.workspace.onDidOpenTextDocument(
+        async (document) => {
+            const { uri } = document;
+            if (uri.scheme === 'git-output' && /\/git-error-/.test(uri.fsPath)) {
+                await vscode.languages.setTextDocumentLanguage(document, 'log');
+            }
         },
+        null,
+        context.subscriptions,
     );
 
-    context.subscriptions.push(disposable);
+    // support output panel syntax highlight
+    vscode.workspace.onDidOpenTextDocument(
+        async (document) => {
+            const { uri } = document;
+            // output channel default languageId is Log and has no highlight
+            // we can change it to log to get log language syntax highlight
+            // NOTE: log !== Log
+            if (uri.scheme === 'output' && document.languageId === 'Log') {
+                await vscode.languages.setTextDocumentLanguage(document, 'log');
+            }
+        },
+        null,
+        context.subscriptions,
+    );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
